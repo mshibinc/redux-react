46921d5571e34933407a9cd67c21ff36f35ef0a8
mshibinc <38204529+mshibinc@users.noreply.github.com>
2023-03-13 12:35:53 +0530

    after redux cart function

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addTocart, increment, decrement } from "./redux/cart";
 export default function Cart({ name, id }) {
   const dispatch = useDispatch();
   const { cartList, cartCount } = useSelector((state) => state.cart);
+  const productCartCount = useMemo(() => {
+    return cartList?.find((itm) => id === itm?.id)?.count;
+  }, [cartList]);
   return (
     <div className="App">
       <h4>{name}</h4>
-      {cartCount == 0 ? (
+      {productCartCount > 0 ? (
+        <>
+          <button onClick={() => dispatch(decrement(id))}>-</button>
+          <span>{productCartCount}</span>
+          <button onClick={() => dispatch(increment(id))}>+</button>
+        </>
+      ) : (
         <button
           onClick={() => {
             dispatch(addTocart({ name, id }));
@@ -15,12 +24,6 @@ export default function Cart({ name, id }) {
         >
           Add to cart
         </button>
-      ) : (
-        <>
-          <button onClick={() => dispatch(decrement())}>-</button>
-          <span>{cartCount}</span>
-          <button onClick={() => dispatch(increment())}>+</button>
-        </>
       )}
     </div>
   );
--- src/redux/cart.js
+++ src/redux/cart.js
@@ -1,28 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 const INITIAL_STATE = {
   cartList: [],
-  cartCount: 0
+  cartCount: 0,
 };
 const cartSlice = createSlice({
   name: "cart",
   initialState: INITIAL_STATE,
   reducers: {
     addTocart: (state, action) => {
-      // sate.cartCount = 1;
-      //action.type
-      // console.log(state.cartList);
-      state.cartList.push(action.payload);
+      const itemExist = state.cartList.find(
+        (itm) => itm.id === action.payload.id
+      );
+      if (itemExist) {
+        state.cartList.forEach((itm) => {
+          if (itm.id === action.payload.id) {
+            itm.count++;
+          }
+        });
+      } else {
+        state.cartList.push({ ...action.payload, count: 1 });
+      }
     },
-    increment: (state) => {
-      state.cartCount += 1;
+    increment: (state, action) => {
+      let pid = action.payload;
+      state.cartList.forEach((itm) => {
+        if (itm.id === pid) {
+          itm.count++;
+        }
+      });
     },
-    decrement: (state) => {
-      state.cartCount -= 1;
+    decrement: (state, action) => {
+      let pid = action.payload;
+      state.cartList.forEach((itm) => {
+        if (itm.id === pid) {
+          itm.count--;
+        }
+      });
     },
-    removeCart: (state) => {
+    removeCart: (state, action) => {
       state.cartCount = 0;
-    }
-  }
+    },
+  },
 });
 export const { increment, decrement, addTocart } = cartSlice.actions;
 export default cartSlice.reducer;
